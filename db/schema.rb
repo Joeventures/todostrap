aa1cffe0b5cd985d65bdf6b6fb00046e880c35a6
Joe Winter <joe@joeventures.com>
2015-12-27 17:44:25 -0500

    Todos Scaffold schema migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227181637) do
+ActiveRecord::Schema.define(version: 20151227224032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "title"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
